75dd10fda2ca6fef7081c875cc107d288b160792
Petr Bui <buihonganh1@gmail.com>
2020-10-29 18:39:24 +0100

    oprava a instalace @types/react

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,18 @@
 import React from "react";
 import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "react-apollo";
 import "./App.css";
 
 const client = new ApolloClient({
-uri:"https://countries.trevorblades.com/"
+  uri: "https://countries.trevorblades.com/",
 });
 
-function App() {
-  return (
-    <div className='App'>
-      <header className='App-header'>
-       
-      </header>
+const App = () => (
+  <ApolloProvider client={client}>
+    <div>
+      <h1>Domaci ukol</h1>
     </div>
-  );
-}
+  </ApolloProvider>
+);
 
 export default App;
